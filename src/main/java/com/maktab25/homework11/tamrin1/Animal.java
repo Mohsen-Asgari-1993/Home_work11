4cb94dcba5b05d2a8a0c4d917197d62f53e09b12
mohsen <mohsen1993asgari@gmail.com>
2019-06-08 09:32:51 +0430

    Creates Animal Class

---
+++ src/main/java/com/maktab25/homework11/tamrin1/Animal.java
@@ -0,0 +1,17 @@
+package com.maktab25.homework11.tamrin1;
+
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
