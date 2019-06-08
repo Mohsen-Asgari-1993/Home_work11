89120ae32f59ea321daf45642c116cf54e703d13
mohsen <mohsen1993asgari@gmail.com>
2019-06-08 09:41:41 +0430

    Adds filter method

--- src/main/java/com/maktab25/homework11/tamrin1/SecondMain.java
+++ src/main/java/com/maktab25/homework11/tamrin1/SecondMain.java
@@ -2,6 +2,8 @@ package com.maktab25.homework11.tamrin1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class SecondMain {
 
@@ -12,5 +14,20 @@ public class SecondMain {
             animalList.add(new Cat("cat" + (i + 1), breedTypes[i]));
         }
         return animalList;
+    }
+
+    private static List<BreedType> filterByNameAndBreedType(List<Animal> animals) {
+        Function<Cat, BreedType> change = Cat::getBreedType;
+        List<BreedType> catsBreedTypes = animals.stream().filter(cat -> {
+            if (cat instanceof Cat) {
+                if (((Cat) cat).getBreedType().toString().startsWith("A") ||
+                        ((Cat) cat).getBreedType().toString().startsWith("C") ||
+                        ((Cat) cat).getBreedType().toString().startsWith("P"))
+                    return true;
+            }
+            return false;
+        }).filter(cat -> Integer.parseInt(cat.getName().substring(3)) % 2 == 0)
+                .map(c -> change.apply((Cat) c)).collect(Collectors.toList());
+        return catsBreedTypes;
     }
 }
