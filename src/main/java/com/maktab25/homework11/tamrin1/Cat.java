f8e4831962fcaeedc2c25e414a893ba72928bc3b
mohsen <mohsen1993asgari@gmail.com>
2019-06-08 09:37:27 +0430

    Creates Cat Class

---
+++ src/main/java/com/maktab25/homework11/tamrin1/Cat.java
@@ -0,0 +1,27 @@
+package com.maktab25.homework11.tamrin1;
+
+public class Cat extends Animal {
+    private BreedType breedType;
+
+    public Cat(String name, BreedType breedType) {
+        super(name);
+        this.breedType = breedType;
+    }
+
+    public BreedType getBreedType() {
+        return breedType;
+    }
+
+    public void setBreedType(BreedType breedType) {
+        this.breedType = breedType;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                " name= " + super.getName() +
+                ", breedType=" + breedType +
+                '}';
+    }
+}
+
