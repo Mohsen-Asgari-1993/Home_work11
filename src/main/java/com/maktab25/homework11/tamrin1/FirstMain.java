1db7046a7608943fdb4ccf81eb4af8d2245f99af
mohsen <mohsen1993asgari@gmail.com>
2019-06-08 09:38:37 +0430

    adds 99 cat in a list

--- src/main/java/com/maktab25/homework11/tamrin1/FirstMain.java
+++ src/main/java/com/maktab25/homework11/tamrin1/FirstMain.java
@@ -1,7 +1,61 @@
 package com.maktab25.homework11.tamrin1;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FirstMain {
     public static void main(String[] args) {
+        List<Animal> asfasf = Arrays.asList(
+                new Cat("cat1", BreedType.Abyssinian), new Cat("cat2", BreedType.Aegean),
+                new Cat("cat3", BreedType.American_Bobtail), new Cat("cat4", BreedType.American_Curl),
+                new Cat("cat5", BreedType.American_Shorthair), new Cat("cat6", BreedType.American_Wirehair),
+                new Cat("cat7", BreedType.Aphrodite_Giant), new Cat("cat8", BreedType.Arabian_Mau),
+                new Cat("cat9", BreedType.Asian), new Cat("cat10", BreedType.Asian_Semilonghair),
+                new Cat("cat11", BreedType.Australian_Mist), new Cat("cat12", BreedType.Balinese),
+                new Cat("cat13", BreedType.Bambino), new Cat("cat14", BreedType.Bengal),
+                new Cat("cat15", BreedType.Birman), new Cat("cat16", BreedType.Bombay),
+                new Cat("cat17", BreedType.Brazilian_Shorthair), new Cat("cat18", BreedType.British_Longhair),
+                new Cat("cat19", BreedType.British_Semilonghair), new Cat("cat20", BreedType.British_Shorthair),
+                new Cat("cat21", BreedType.Burmese), new Cat("cat22", BreedType.Burmilla),
+                new Cat("cat23", BreedType.California_Spangled), new Cat("cat24", BreedType.Chantilly_Tiffany),
+                new Cat("cat25", BreedType.Chartreux), new Cat("cat26", BreedType.Chausie),
+                new Cat("cat27", BreedType.Cheetoh), new Cat("cat28", BreedType.Colorpoint_Shorthair),
+                new Cat("cat29", BreedType.Cornish_Rex), new Cat("cat30", BreedType.Cymric),
+                new Cat("cat31", BreedType.Cyprus), new Cat("cat32", BreedType.Donskoy),
+                new Cat("cat33", BreedType.Devon_Rex), new Cat("cat34", BreedType.Dragon_Li),
+                new Cat("cat35", BreedType.Dwelf), new Cat("cat36", BreedType.Egyptian_Mau),
+                new Cat("cat37", BreedType.European_Shorthair), new Cat("cat38", BreedType.Exotic_Shorthair),
+                new Cat("cat39", BreedType.Foldex), new Cat("cat40", BreedType.German_Rex),
+                new Cat("cat41", BreedType.Havana_Brown), new Cat("cat42", BreedType.Highlander),
+                new Cat("cat43", BreedType.Himalayan), new Cat("cat44", BreedType.Japanese_Bobtail),
+                new Cat("cat45", BreedType.Javanese), new Cat("cat46", BreedType.Karelian_Bobtail),
+                new Cat("cat47", BreedType.Khao_Manee), new Cat("cat48", BreedType.Korat),
+                new Cat("cat49", BreedType.Korean_Bobtail), new Cat("cat50", BreedType.Korn_Ja),
+                new Cat("cat51", BreedType.Kurilian_Bobtail), new Cat("cat52", BreedType.Maine_Coon),
+                new Cat("cat53", BreedType.Manx), new Cat("cat54", BreedType.Mekong_Bobtail),
+                new Cat("cat55", BreedType.Minskin), new Cat("cat56", BreedType.Munchkin),
+                new Cat("cat57", BreedType.LaPerm), new Cat("cat58", BreedType.Lykoi),
+                new Cat("cat59", BreedType.Napoleon), new Cat("cat60", BreedType.Nebelung),
+                new Cat("cat61", BreedType.Norwegian_Forest_Cat), new Cat("cat62", BreedType.Ocicat),
+                new Cat("cat63", BreedType.Ojos_Azules), new Cat("cat64", BreedType.Oregon_Rex),
+                new Cat("cat65", BreedType.Oriental_Bicolor), new Cat("cat66", BreedType.Oriental_Longhair),
+                new Cat("cat67", BreedType.Oriental_Shorthair), new Cat("cat68", BreedType.Persian_Modern),
+                new Cat("cat69", BreedType.Persian_Traditional), new Cat("cat70", BreedType.Peterbald),
+                new Cat("cat71", BreedType.Pixie_Bob), new Cat("cat72", BreedType.Raas),
+                new Cat("cat73", BreedType.Ragamuffin), new Cat("cat74", BreedType.Ragdoll),
+                new Cat("cat75", BreedType.Russian_Blue), new Cat("cat76", BreedType.Russian_White),
+                new Cat("cat77", BreedType.Sam_Sawet), new Cat("cat78", BreedType.Savannah),
+                new Cat("cat79", BreedType.Scottish_Fold), new Cat("cat80", BreedType.Selkirk_Rex),
+                new Cat("cat81", BreedType.Serengeti), new Cat("cat82", BreedType.Serrade_Petit),
+                new Cat("cat83", BreedType.Siamese), new Cat("cat84", BreedType.Siberian),
+                new Cat("cat85", BreedType.Singapura), new Cat("cat86", BreedType.Snowshoe),
+                new Cat("cat87", BreedType.Sokoke), new Cat("cat88", BreedType.Somali),
+                new Cat("cat89", BreedType.Sphynx), new Cat("cat90", BreedType.Suphalak),
+                new Cat("cat91", BreedType.Ukrainian_Levkoy), new Cat("cat92", BreedType.Wila_Krungthep),
+                new Cat("cat93", BreedType.YorkChocolate), new Cat("cat94", BreedType.Tonkinese),
+                new Cat("cat95", BreedType.Thai_Lilac), new Cat("cat96", BreedType.Thai),
+                new Cat("cat97", BreedType.Turkish_Angora), new Cat("cat98", BreedType.Toyger),
+                new Cat("cat99", BreedType.Turkish_Van));
 
     }
 }
