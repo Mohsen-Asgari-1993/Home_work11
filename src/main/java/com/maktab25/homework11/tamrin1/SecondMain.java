60e727c9b551456d930d678cc6aae25715dc61d0
mohsen <mohsen1993asgari@gmail.com>
2019-06-08 09:40:59 +0430

    Adds initList Method

--- src/main/java/com/maktab25/homework11/tamrin1/SecondMain.java
+++ src/main/java/com/maktab25/homework11/tamrin1/SecondMain.java
@@ -1,4 +1,16 @@
 package com.maktab25.homework11.tamrin1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SecondMain {
+
+    private static List<Animal> initList() {
+        List<Animal> animalList = new ArrayList<>();
+        BreedType[] breedTypes = BreedType.values();
+        for (int i = 0; i < 99; i++) {
+            animalList.add(new Cat("cat" + (i + 1), breedTypes[i]));
+        }
+        return animalList;
+    }
 }
