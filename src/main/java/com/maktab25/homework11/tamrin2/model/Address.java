1f7100e67074fb850072fd65205e6d2fc01038a2
mohsen <mohsen1993asgari@gmail.com>
2019-06-08 10:29:25 +0430

    Creates Address class

---
+++ src/main/java/com/maktab25/homework11/tamrin2/model/Address.java
@@ -0,0 +1,64 @@
+package com.maktab25.homework11.tamrin2.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "address")
+@Setter
+@Getter
+@NoArgsConstructor
+public class Address implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column(name = "postal_address")
+    private String postalAddress;
+
+    @Column(name = "city")
+    private String city;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,
+            CascadeType.MERGE,
+            CascadeType.PERSIST,
+            CascadeType.REFRESH})
+    @JoinColumn(name = "emp_id")
+    private Employee employee;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PhoneNumber> numbers;
+
+    public Address(String postalCode, String postalAddress, String city, List<PhoneNumber> numbers) {
+        this.postalCode = postalCode;
+        this.postalAddress = postalAddress;
+        this.city = city;
+        this.numbers = numbers;
+    }
+
+    public void addNumbers(PhoneNumber phoneNumber) {
+        if (numbers == null)
+            numbers = new ArrayList<>();
+        numbers.add(phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "postalCode='" + postalCode + '\'' +
+                ", postalAddress='" + postalAddress + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
